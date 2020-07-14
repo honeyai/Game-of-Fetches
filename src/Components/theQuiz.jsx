2166d11367bc01c9ac036ea4acf66c7ef93a4ee8
honeyai <contact.jessicasanchez@gmail.com>
2020-07-14 15:07:25 -0400

    Starting App

---
+++ src/Components/theQuiz.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class theQuiz extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default theQuiz;
